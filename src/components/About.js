57c7c2ff7956fbc0b5a310cd48163b054711604c
nainoktv <nainoaktvillegas@gmail.com>
2022-10-29 16:15:23 -0700

    feat: TODO Carousel for Welcome Page

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
-            <p>This is about us...</p>
+            <h1>What is Generous?</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Tempore dolorum excepturi, pariatur dolore fuga perferendis 
                 mollitia deserunt a voluptas assumenda! Consequatur beatae qui 
