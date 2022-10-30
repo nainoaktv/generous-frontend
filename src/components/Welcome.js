fdef074421a3dcfbeb621ab3bc10b4527adc1365
nainoktv <nainoaktvillegas@gmail.com>
2022-10-29 17:38:10 -0700

    feat: FIX Carousel on Welcome page & add scripts index.html

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import ControlledCarousel from './Carousel';
+import HomeCarousel from './Carousel';
 
 const Welcome = () => {
   return (
     <div>
-      <ControlledCarousel />
+      <HomeCarousel />
     </div>
   )
 }
