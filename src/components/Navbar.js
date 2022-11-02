a8238f4a0df737bc817c3a31264c14eb499be8b2
nainoktv <nainoaktvillegas@gmail.com>
2022-11-02 03:21:11 -0700

    feat: ADD functional side nav menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
-import { NavLink, Link } from 'react-router-dom';
+// import { NavLink, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
+import { IconContext } from "react-icons";
 import { NavbarData } from './NavbarData';
+import './Navbar.css';
 
 const Navbar = (props) => {
 
@@ -11,32 +14,32 @@ const Navbar = (props) => {
   const showSideBar = () => setSidebar(!sidebar);
 
   return (
-    <>
-      <div className='navbar'>
-        <Link to='#' className='menu-bars'>
-          <FaIcons.FaBars onClick={showSideBar} />
-        </Link>    
-      </div>
-      <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-        <ul className='nav-menu-items'>
-          <li className='navbar-toggle'>
-            <Link to='#' className='menu-bars'>
-              <AiIcons.AiOutlineClose />
-            </Link>
-          </li>
-          {NavbarData.map((item, index) => {
-            return (
-              <li key={index} className={item.cName}>
-                <Link to={item.path}>
-                  {item.icon}
-                  <span>{item.title}</span>
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-      </nav>
-    </>
+      <IconContext.Provider value={{ color: 'black' }}>
+        <div className='navbar'>
+          <Link to='#' className='menu-bars'>
+            <FaIcons.FaBars onClick={showSideBar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSideBar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {NavbarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
   );
 }
 
