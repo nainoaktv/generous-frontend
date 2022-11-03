8bee61bd1bcd6a6ee1bd832bcf716c1729f72e4c
nainoktv <nainoaktvillegas@gmail.com>
2022-11-02 22:01:47 -0700

    chore: add generous logo to welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import HomeCarousel from './Carousel';
+import './Welcome.css';
 
 const Welcome = () => {
   return (
     <div>
+      <img className='genLogo' src={ require('../images/genlogo.png').default } />
       <HomeCarousel />
     </div>
   )
